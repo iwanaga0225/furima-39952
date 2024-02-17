56b8a569894e38155498a92c7072a7f9643d6bed
iwanaga0225 <n.iwanaga.amethyst@gmail.com>
2024-02-17 11:17:49 +0900

    単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :items
+  has_many :orders
+
+
+  validates :nickname,           presence: true
+  validates :email,              presence: true
+  validates :encrypted_password, presence: true, length: { minimum: 6 }
+  validates :first_name,         presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name,          presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kana,    presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_kana,     presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday,           presence: true
+
+
 end
