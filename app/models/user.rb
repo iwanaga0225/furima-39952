9aeef39076dd7b8ce864f5216ca44ea4dc5a0385
iwanaga0225 <n.iwanaga.amethyst@gmail.com>
2024-02-20 21:18:24 +0900

    ユーザー管理機能のプルリクエスト修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
 
   validates :nickname,           presence: true
-  validates :password,           format: { with: VALID_PASSWORD_REGEX, message: 'must contain both half-width alphanumeric characters' }, length: { minimum: 6 }
+  validates :password,           format: { with: VALID_PASSWORD_REGEX, message: 'must contain both half-width alphanumeric characters' }
   validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'please input your name using full-width characters (Kanji, Hiragana, or Katakana)' }
   validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'please input your name using full-width characters (Kanji, Hiragana, or Katakana)' }
   validates :first_name_kana,    presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'please input your name_kana using full-width Katakana characters' }
