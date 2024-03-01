2e5c9b1241f5dd0a136459dc83293722a8f8d7c0
iwanaga0225 <n.iwanaga.amethyst@gmail.com>
2024-03-01 15:19:09 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
   validates :image,              presence: true
   validates :name,               presence: true
   validates :content,            presence: true
-  validates :category_id,        numericality: { other_than: 1 }
-  validates :status_id,          numericality: { other_than: 1 }
-  validates :delivery_charge_id, numericality: { other_than: 1 }
-  validates :delivery_area_id,   numericality: { other_than: 1 }
-  validates :delivery_date_id,   numericality: { other_than: 1 }
+  validates :category_id,        numericality: { other_than: 1, message: "can't be blank"}
+  validates :status_id,          numericality: { other_than: 1, message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :delivery_area_id,   numericality: { other_than: 1, message: "can't be blank"}
+  validates :delivery_date_id,   numericality: { other_than: 1, message: "can't be blank"}
   validates :price,              presence: true
 
   belongs_to :user
