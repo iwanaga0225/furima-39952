a2c50e5973eb8b79b839977e5960f28a219474dd
iwanaga0225 <n.iwanaga.amethyst@gmail.com>
2024-03-05 21:40:42 +0900

    商品一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,  except: [:index, :show]
+  #before_action :authenticate_user!,  except: [:index, :show]
   
   def index
     @items = Item.all.order(created_at: "DESC")
@@ -19,10 +19,8 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
-  
   private
 
   def item_params
